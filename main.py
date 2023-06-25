8e691f6db6156c7728bbeaf1bcd67266e9c7ad7c
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2023-06-25 22:20:03 +0000

    worked on settings stuff

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import simpledialog
 
 class Menu: #Main menu class
     def __init__(self):
+        self.name=User.username #work plz
         self.menu=tk.Tk()
         self.menu.title("Main Menu")
         self.menu.geometry("400x300")
         
         self.label = tk.Label(self.menu, text="MAIN MENU")
-
         self.label.pack()
+        self.label2 = tk.Label(self.menu, text=self.name) #work plz
     
         self.menubts = tk.Frame(self.menu, bg="green")
         self.menubts.place(relx = .5, rely = .5, anchor="center")
@@ -25,6 +27,7 @@ class Menu: #Main menu class
 
         self.menu.protocol("WM_DELETE_WINDOW", self.exit)
         self.menu.mainloop()
+        
     def exit(self):
         if messagebox.askyesno(title="Exit", message="Are you sure?"):
             self.menu.destroy()
@@ -32,19 +35,19 @@ class Menu: #Main menu class
 class Settings:
     def __init__(self):
         
-        option=tk.Tk()
-        option.title("Settings")
-        option.geometry("200x200")
-        obt1=tk.Button(option, text="Exit", command=option.destroy)
-        obt1.place(relx=0.5, rely=0.9, anchor="s")
-        obt2=tk.Button(option, text="Name change", command=User)
-        obt2.pack()
-        obt3=tk.Button(option, text="nill", command="nill")
-        obt3.pack()
-
+        self.option=tk.Tk()
+        self.option.title("Settings")
+        self.option.geometry("200x200")
+        self.obt1=tk.Button(self.option, text="Exit", command=self.option.destroy)
+        self.obt1.place(relx=0.5, rely=0.9, anchor="s")
+        self.obt2=tk.Button(self.option, text="Name change", command=User)
+        self.obt2.pack()
+        self.obt3=tk.Button(self.option, text="nill", command="nill")
+        self.obt3.pack()
 
 class User:
-    pass
-
+    def __init__(self, username): #work plz
+        self.name = simpledialog.askstring("Username", "What is your name?")
+        self.name = username
 
 Menu()
