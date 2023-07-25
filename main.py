ba43e698edd68ac8eb550d7f4a46031bd36cb0c3
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2023-07-25 00:45:21 +0000

    Complete redesign and restructure of my code

--- main.py
+++ main.py
@@ -1,62 +1,36 @@
-import tkinter as tk
-from tkinter import messagebox
-from tkinter import simpledialog
+import tkinter as tk # These import tkinter
+from tkinter import messagebox # This imports the messagebox function in tkinter
+from tkinter import simpledialog # This imports the Simpledialog function from tkinter
 
+class Nill:
+    print("Does Nothing")
 
-class User:
+class Error:
     def __init__(self):
-        self.username_change()
-        self.Uname = "none"
-        
-    def username_change(self):
-        self.Uname = simpledialog.askstring( "Username", "What is your name?")
+        tk.messagebox(title="Error", message="There has been an error")
 
-    def __str__(self):
-        return self.Uname
+class Window: # This class is for creating a basic window
+    def __init__(self, title, size): # This is the definition that is called when the class is initilized
+        self.title = title # This is the varable for the title bar of the window
+        self.size = size # This is the variable for setting the size of the window
+        self.window = tk.Tk() # This makes the window
+        self.window.title(self.title) # This sets the title of the window
+        self.window.geometry(self.size) # This sets the size of the window
+        self.label = tk.Label(self.window, text=self.title)
+        self.label.pack(pady=5)
+        self.window.protocol("WM_DELETE_WINDOW", self.exit)
         
-class Menu: #Main menu class
-    def __init__(self):
-        self.name = User() #bruh
-        self.menu=tk.Tk()
-        self.menu.title("Main Menu")
-        self.menu.geometry("400x300")
-        
-        self.label = tk.Label(self.menu, text="MAIN MENU")
-        self.label.pack()
-        self.namelabel = tk.Label(self.menu, text="Username: {}".format(self.name))
-        self.namelabel.pack()
-        
-        self.menubts = tk.Frame(self.menu, bg="green")
-        self.menubts.place(relx = .5, rely = .5, anchor="center")
-        
-        self.bt1 = tk.Button(self.menubts, text="Play")
-        self.bt1.pack(pady=5)
-        
-        self.bt2 = tk.Button(self.menubts, text="Settings", command=Settings)
-        self.bt2.pack(pady=5)
-        
-        self.bt3 = tk.Button(self.menubts, text="Exit", command=self.exit)
-        self.bt3.pack(pady=5)
+    def button(self, text, command): # Definition for adding buttons to the window
+        self.text = text
+        self.command = command
+        self.button = tk.Button(self.window, text=self.text, command=self.command) # Setting what the button does and says
+        self.button.pack(pady=5)
 
-        self.menu.protocol("WM_DELETE_WINDOW", self.exit)
-        self.menu.mainloop()
-        
     def exit(self):
         if messagebox.askyesno(title="Exit", message="Are you sure?"):
-            self.menu.destroy()
-            
-class Settings:
-    def __init__(self):
-        
-        self.option=tk.Tk()
-        self.option.title("Settings")
-        self.option.geometry("200x200")
-        self.obt1=tk.Button(self.option, text="Exit", command=self.option.destroy)
-        self.obt1.place(relx=0.5, rely=0.9, anchor="s")
-        self.obt2=tk.Button(self.option, text="Name change", command=User)
-        self.obt2.pack()
-        self.obt3=tk.Button(self.option, text="nill", command="nill")
-        self.obt3.pack()
+            self.window.destroy()
 
 
-Menu()
+
+menu = Window("Menu", "400x300").button("Play", "Nill")
+menu
