a723637ffdfd45b33b43383e95327ee694d16185
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2023-08-01 00:43:02 +0000

    Fixed stuff, added a settings page and a background colour

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ class Error: # This class is for error catching
     def __init__(self):
         tk.messagebox.showerror(title="Error", message="There has been an error")
 
-class Window: # This class is for creating a basic window
-    def __init__(self, title, size): # This is the definition that is called when the class is initilized
+class User:
+    def __init__(self, username):
+        self.username = username
+
+class Window(User): # This class is for creating a basic window
+    def __init__(self, title, size, colour): # This is the definition that is called when the class is initilized
+        self.colour = colour
         self.title = title # This is the varable for the title bar of the window
         self.size = size # This is the variable for setting the size of the window
         self.window = tk.Tk() # This makes the window
         self.window.title(self.title) # This sets the title of the window
         self.window.geometry(self.size) # This sets the size of the window
+        self.window.config(bg=self.colour)
         self.label = tk.Label(self.window, text=self.title)
         self.label.pack(pady=5)
         self.window.protocol("WM_DELETE_WINDOW", self.exit)
@@ -26,11 +32,31 @@ class Window: # This class is for creating a basic window
         self.command = command
         self.button = tk.Button(self.window, text=self.text, command=self.command) # Setting what the button does and says
         self.button.pack(pady=5)
-        self.window.mainloop()
+        
+    def button2(self, text, command):
+        self.text = text
+        self.command = command
+        self.button = tk.Button(self.window, text=self.text, command=self.command)
+        self.button.pack(pady=5)
+        
+    def button3(self, text, command):
+        self.text = text
+        self.command = command
+        self.button = tk.Button(self.window, text=self.text, command=self.command)
+        self.button.pack(pady=5)
+    
     def exit(self):
         if messagebox.askyesno(title="Exit", message="Are you sure?"):
             self.window.destroy()
 
+class Settings:
+    def __init__(self):
+        self.settings = Window("Settings", "300x300", "blue")
+        self.settings.button("Change Name", Nill)
+        self.settings.button2("Exit", self.settings.exit)
 
-
-menu = Window("Menu", "400x300").button("Test Button", Error)
+menu = Window("Menu", "400x300", "white")
+menu.button("Play", Error)
+menu.button2("Settings", Settings)
+menu.button3("Exit", menu.exit)
+menu.window.mainloop()
