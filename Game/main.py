0ec7cf9d28bd563b0353ed4e873102358e7f1e1a
PotatoPriest <79816096+PotatoPriest@users.noreply.github.com>
2023-08-31 02:30:34 +0000

    Drop down menu for picking what to do

--- Game/main.py
+++ Game/main.py
@@ -1,12 +1,14 @@
 import tkinter as tk #importing stuff
 from tkinter import messagebox
 from tkinter import simpledialog
+from tkinter import StringVar
+option_menu = ["Empty", "Math", "Geograhy", "Trivia"]
 
 def button(master, text, command): # definition for making a button
     bt = tk.Button(master, text=text, command=command)
     bt.pack(pady=5)
 
-def label(master, text):
+def label(master, text): # definition for label
     la = tk.Label(master, text=text)
     la.pack(pady=5)
 
@@ -40,20 +42,50 @@ class Window: # class for the main window
             self.window.destroy()
             
     def start(self): # this starts the game
+        menu = StringVar()
+        menu.set("Empty")
         self.menu_things.destroy()
         self.setting_things = tk.Frame(self.window)
         self.label = tk.Label(self.setting_things, text="Name: {}".format(self.name)) # I want to use the function but I dont know how to edit the label when its in a function.
         self.label.pack(pady=5)
         button(self.setting_things, "Set your name", self.set_name)
-        label(self.setting_things, "hmm")
+        label(self.setting_things, "What are you going to play?")
+        self.play_option = tk.OptionMenu(self.setting_things, menu, *option_menu)
+        self.play_option.pack(pady=5)
+        button(self.setting_things, "Continue", self.next)
         self.setting_things.pack(pady=5)
-        
-    def set_name(self):
+
+    def next(self):
+        if self.play_option == "Empty":
+            self.empty
+        elif self.play_option == "Math":
+            self.math
+            print("Math")
+        elif self.play_option == "Geography":
+            self.geography
+        elif self.play_option == "Trivia":
+            self.trivia
+        else:
+            print("bruh")
+    
+    def set_name(self): #sets name and saves name
         self.name = simpledialog.askstring("Pick your Name", "Name:")
         self.save_file = open("Game/save.txt", "w")
         self.save_file.writelines([self.name])
         self.save_file.close()
         self.label.config(text="Name: " + self.name)
+
+    def empty(self):
+        pass
+    
+    def math(self):
+        self.setting_things.destroy()
+
+    def geography(self):
+        self.setting_things.destroy()
+
+    def trivia(self):
+        self.setting_things.destroy()
 
 
 window = Window()
